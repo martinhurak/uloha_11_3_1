35922534dfc9157f317a9320f753f2e9b2d61036
martinhurak <95643917+martinhurak@users.noreply.github.com>
2022-05-03 11:20:17 +0200

    Add files via upload

---
+++ JavaScript/script.js
@@ -0,0 +1,24 @@
+
+function uloha(x=4,y=2){
+
+var z = 0    
+ console.log ("prve čislo bolo zadane "+ x +" druhe čislo bolo zadane "+y);
+ console.log(" Vykonávam matematické operácie");
+
+//scitanie
+z = x+y
+ console.log(x+"+"+y+"="+z)
+//odcitanie   
+z = x-y
+console.log(x+"-"+y+"="+z)
+//nasobenie
+z = x*y
+console.log(x+"*"+y+"="+z)
+//delenie
+z = x/y
+console.log(x+"/"+y+"="+z)
+   }
+
+  
+uloha()
+uloha (5,8)
